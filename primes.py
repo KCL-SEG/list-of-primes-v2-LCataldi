1e51c1dfb13bfbc507321e0720940c89e11a11aa
LorenzoC <lorenzo.cataldi@icloud.com>
2023-11-29 18:29:07 +0000

    Check primes() function

--- primes.py
+++ primes.py
@@ -3,4 +3,23 @@
 
 def primes(number_of_primes):
     list = []
+
+    if number_of_primes <= 0:
+        raise ValueError
+
+    number_to_check = 0
+    while len(list) != number_of_primes:
+
+        is_prime = True
+        if number_to_check <= 1:
+            is_prime = False
+        for i in range(2, int(number_to_check ** 0.5) + 1):
+            if number_to_check % i == 0:
+                is_prime = False
+
+        if is_prime:
+            list.append(number_to_check)
+
+        number_to_check = number_to_check + 1
+
     return list
